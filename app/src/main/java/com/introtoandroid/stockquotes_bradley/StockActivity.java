0730c7932f5a385788fa23942ec2509cbc1e081d
Luke Bradley <bradleyla@g.cofc.edu>
2017-05-05 00:07:46 -0400

    App crashes whenever I try to input a stock company symbol.

--- app/src/main/java/com/introtoandroid/stockquotes_bradley/StockActivity.java
+++ app/src/main/java/com/introtoandroid/stockquotes_bradley/StockActivity.java
@@ -2,8 +2,14 @@ package com.introtoandroid.stockquotes_bradley;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.view.KeyEvent;
+import android.text.TextWatcher;
+import android.text.Editable;
+import android.widget.Toast;
+import android.os.AsyncTask;
+import java.io.IOException;
 
 public class StockActivity extends AppCompatActivity {
 
@@ -24,6 +30,7 @@ public class StockActivity extends AppCompatActivity {
 
     public boolean search = false;
 
+    public String currSymbol;
     public String symbol;
     public String name;
     public String tradePrice;
@@ -52,13 +59,47 @@ public class StockActivity extends AppCompatActivity {
         rangeOut = (TextView) findViewById(R.id.rangeOut);
         symbolInput = (EditText) findViewById(R.id.symbolInput);
 
+        symbolInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if (s.length() > 0) {
+                    currSymbol = s.toString();
+                }
+                else {
+                    currSymbol = "";
+                }
+            }
+        });
+
+        symbolInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                readStock stockData = new readStock();
+                stockData.execute(currSymbol);
+                return handled;
+            }
+        });
 
 
     }
 
+    @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putBoolean("search", search);
+        outState.putString("currSymbol", currSymbol);
         outState.putString("symbol", symbol);
         outState.putString("name", name);
         outState.putString("tradePrice", tradePrice);
@@ -67,6 +108,7 @@ public class StockActivity extends AppCompatActivity {
         outState.putString("range", range);
     }
 
+    @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
@@ -78,11 +120,52 @@ public class StockActivity extends AppCompatActivity {
         change = savedInstanceState.getString("change");
         range = savedInstanceState.getString("range");
 
+        symbolInput.setText(currSymbol);
+
         symbolOut.setText(symbol);
         nameOut.setText(name);
         tradePriceOut.setText(tradePrice);
         tradeTimeOut.setText(tradeTime);
         changeOut.setText(change);
         rangeOut.setText(range);
+    }
+
+    private class readStock extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        protected String doInBackground(String... params) {
+            Stock data = new Stock(currSymbol);
+            String done = "done";
+            try {
+                data.load();
+            }
+            catch (IOException i) {
+            }
+
+            symbol = data.getSymbol();
+            name = data.getName();
+            tradePrice = data.getLastTradePrice();
+            tradeTime = data.getLastTradeTime();
+            change = data.getChange();
+            range = data.getRange();
+            return done;
+        }
+
+        @Override
+        protected void onPostExecute(String done) {
+            super.onPostExecute(done);
+            symbolOut.setText(symbol);
+            nameOut.setText(name);
+            tradePriceOut.setText(tradePrice);
+            tradeTimeOut.setText(tradeTime);
+            changeOut.setText(change);
+            rangeOut.setText(range);
+
+
+        }
     }
 }
